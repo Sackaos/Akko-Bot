a36cfad29235df08162b4f0a817f719a9f5e5c82
Sackaos <Firedragon3009@gmail.com>
2021-01-29 14:16:43 +0200

    microchanges

--- src/discordUtility.js
+++ src/discordUtility.js
@@ -1,32 +1,5 @@
 const data = require("./data.js");
 
-//TEMP FOR REF SEND FILE -- IMAGE
-// const sendImage = () => {
-//   messageObj.channel.send("img", {
-//     embed: {
-//       author: {
-//         iconURL: "",
-//         username: "Sackaos",
-//         url: "https://discordapp.com/users/358089430814162945",
-//       },
-//       description: "desc descd esccsd",
-//       type: "article",
-//       color: 1000,
-//       title: "poop man",
-//       thumbnail: {
-//         url:
-//           "https://www.metalmetal.co.il/wp-content/uploads/2019/03/skull-icon.jpg",
-//       },
-//       files: [
-//         {
-//           attachment: "img.png",
-//           name: "img.png",
-//         },
-//       ],
-//     },
-//   });
-// };
-
 // PRIVATE FUNCTION
 
 //PUBLIC FUNCTION
